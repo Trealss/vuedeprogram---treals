46438bd5d4239dae4917dd90b445d45be1d69e4d
trealss <3077371412@qq.com>
2019-03-21 12:08:01 +0800

    vueprogramsbytreals

---
+++ vueday08/vue-program/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.app {
+  padding-top: 46px;
+}
+</style>
